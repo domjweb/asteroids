6e18f5b3f182d1d1e9557fdbbd1b443e470d4091
domjweb <dominiquejadonwebb@gmail.com>
2025-05-27 11:41:58 -0400

    Added player triangle/circle

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
 	pygame.init()
+	player1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	while True:
 		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				return
+			 if event.type == pygame.QUIT:
+					return
 		screen.fill((0,0,0))
+		player1.draw(screen)
 		pygame.display.flip()
+
+	
 	print(f"""Starting Asteroids!
 Screen width: {SCREEN_WIDTH}
 Screen height: {SCREEN_HEIGHT}""")
