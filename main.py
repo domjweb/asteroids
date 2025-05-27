c9343e83a9cdcb568805a88d39c58d25851bf000
domjweb <dominiquejadonwebb@gmail.com>
2025-05-27 17:05:20 -0400

    Added player movement and groups

--- main.py
+++ main.py
@@ -4,17 +4,22 @@ from player import *
 
 def main():
 	pygame.init()
-	player1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 	dt = 0
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	fps_clock = pygame.time.Clock()
+	updateable = pygame.sprite.Group()
+	drawables = pygame.sprite.Group()
+	Player.containers = (updateable, drawables)
+	player1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
 	while True:
 		for event in pygame.event.get():
 			 if event.type == pygame.QUIT:
 					return
 		screen.fill((0,0,0))
-		player1.update(dt)
-		player1.draw(screen)
+		updateable.update(dt)
+		for drawable in drawables:
+			drawable.draw(screen)
 		pygame.display.flip()
 		dt = fps_clock.tick(60) / 1000
 
